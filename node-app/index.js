1290dc91596ca9e12d23d3ab11be9406899dca91
Quyen Tran <t.quyen1997@yahoo.com>
2019-06-24 17:07:39 -0500

    Add files via upload

---
+++ node-app/index.js
@@ -0,0 +1,9 @@
+var lion = require ('lion-lib-89898');
+var add = lion.add(2,3);
+console.log(add);
+
+var multiply = lion.multiply(3,4);
+console.log(multiply);
+
+var sub = lion.subtract(2,5);
+console.log(sub);
